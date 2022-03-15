52f544d1ea6f93118ff749f1b8fa0175dbbb4b25
lyaef <lyaef58@gmail.com>
2022-03-15 13:24:29 -0700

    My seventh commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,15 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-=_{
-char putchar[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
-unsigned int i;
-for (i = 0; i < sizeof(putchar); i++)
 {
-_putchar(putchar[i]);
-}
-_putchar(10);
+ _putchar('_');
+ _putchar('p');
+ _putchar('u');
+ _putchar('t');
+ _putchar('c');
+ _putchar('h');
+ _putchar('a');
+ _putchar('r');
+ _putchar('\n');
 return (0);
-{
+}
