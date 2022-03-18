6e1d2e9a87edc341115970f6afe05cb2e18e632d
lyaef <lyaef58@gmail.com>
2022-03-18 16:35:25 -0700

    My nineth commit

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  */
 void print_triangle(int size)
 {
-int i = 0, j, n = size -1;
+int i = 0, j, n = size - 1;
 
 if (size > 0)
 {
@@ -18,7 +18,7 @@ for (j = 0; j < size; j++)
 if (j < n)
 _putchar(' ');
 else
-__putchar('#');
+_putchar('#');
 }
 n--;
 _putchar('\n');
