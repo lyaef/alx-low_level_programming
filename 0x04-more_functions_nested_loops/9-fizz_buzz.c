136425eaabcfc318544a568461d5f1d053f8aec9
lyaef <lyaef58@gmail.com>
2022-03-18 17:32:46 -0700

    My commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* main -Fizz for multiplies of 3 and Buzz for multiples of 5 FizzBuzz for both
+ * main - Fizz for multiplies of 3 and Buzz for multiples of 5 FizzBuzz for both
  *
  *
  * Return: Always 0
@@ -23,11 +23,11 @@ printf("%s", "Fizz");
 }
 else if (x % 5 == 0)
 {
-printf("%s","Buzz");
+printf("%s", "Buzz");
 }
 else
 {
-printf("5d", x);
+printf("%d", x);
 }
 
 if (x !=100)
